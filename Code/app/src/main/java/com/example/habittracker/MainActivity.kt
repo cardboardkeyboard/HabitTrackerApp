84e7393eb33056da616ea2d14b546b3a6f3583ce
cardboardkeyboard <58948218+cardboardkeyboard@users.noreply.github.com>
2025-07-20 14:36:04 -0500

    init commit

---
+++ Code/app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.habittracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.habittracker.ui.theme.HabitTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
