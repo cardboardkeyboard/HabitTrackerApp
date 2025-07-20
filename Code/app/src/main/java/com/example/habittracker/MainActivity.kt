a383595ba308eaedc4ee5ff96cb8a64c238364fc
cardboardkeyboard <58948218+cardboardkeyboard@users.noreply.github.com>
2025-07-20 15:23:13 -0500

    added loading screen

--- Code/app/src/main/java/com/example/habittracker/MainActivity.kt
+++ Code/app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.habittracker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.os.Handler
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +14,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.os.postDelayed
 import com.example.habittracker.ui.theme.HabitTrackerTheme
+//import java.util.logging.Handler
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Handler()   .postDelayed({
+            Log.d("TAG", "Debug message")
+
+            val intent = Intent(this@MainActivity, StartScreen::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
